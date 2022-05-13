d575c264a1a10ce6aa42f0c824f747504495fe3e
中堤 <c-nakatsutsumi@nixe-inc.com>
2022-05-13 19:18:42 +0900

    [WIP] マイグレーションファイルとModelを作成した

--- backend/app/Http/Controllers/TwitterPseudoController.php
+++ backend/app/Http/Controllers/TwitterPseudoController.php
@@ -5,13 +5,26 @@ use Illuminate\Http\Request;
 
 class TwitterPseudoController extends Controller
 {
-    public function Setup(Request $request)
+    public function setup(Request $request)
     {
         return view('TwitterPseudoHome');
     }
 
-    // public function post(Request $request)
-    // {
-    //     return view('');
-    // }
+   public function confirmation(Request $request)
+   {
+       $setup = new Setup;
+       //name属性が'imageup'のinputタグをファイル形式に、画像をpublic/avatarに保存
+       //$image_path = $request->file('imageup')->store('public/avatar/');
+       $username = $request->name;
+       $birhday = $request->birthday;
+       $comment = $request->comment;
+        dd($image_path, $username, $birhday, $comment);
+
+       $setup->image_path = image_path($image_path);
+       $setup->name = $username;
+       $setup->birthday = $birhday;
+       $setup->comment = $comment;
+
+       $setup->save();
+   }
 }
--- backend/resources/views/TwitterPseudoHome.blade.php
+++ backend/resources/views/TwitterPseudoHome.blade.php
@@ -1,27 +1,27 @@
 <html>
     <head>
     <meta charset="utf-8">
-        <title>TwitterPseudon Home</title>
+        <title>TwitterPseudon Setup</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/TwitterPseudo.css') }}">
     </head>
     <body>
         <div class="jumbotron">
-            <h1 class="text-primary display-4 text-center">設定</h1>
+            <h1 class="text-primary display-4 text-center">アカウント設定</h1>
         </div>
-        <form action="/hello" method="post" enctype='multipart/form-data'>
-            <p class="text-center">画像設定</p>
-            <div class="text-center">
-                <input type="file" class="text-center" name="filuup" accept="image/jpeg, image/png">
-            </div>
+        <form action="/SetupConfirmation" method="post" enctype='multipart/form-data'>
+            @csrf
             <br>
-            <p class="text-center">ニックネーム（必須）<br>
+            <p class="text-center font-weight-bold">ニックネーム（必須）<br>
             <input type="text" name="name"></p>
+            <p class="text-center font-weight-bold">生年月日<br>
+            <input type="date" name="birthday"></p>
             <div class="text-center">
-                <p class="text-center">ひとことコメント<br>
+                <p class="text-center font-weight-bold">ひとことコメント<br>
                 <input type="text" class="textarea" name="comment"></p>
                 <input class="btn btn-info" type="submit" value="設定完了">
             </div>
+            {{ csrf_field() }}
         </form>
     </body>
 <html>
