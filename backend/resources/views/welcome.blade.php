f78c5c508b9bb0f95d5e3b73719dd6679a2ec992
NakatsutsumiChikana-NIXE <c-nakatsutsumi@nixe-inc.com>
2024-03-28 16:16:31 +0900

    HTMLに直書きするのではなくcssファイルを使うように修正

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -3,16 +3,16 @@
     <head>
         <meta charset="utf-8">
         <title>Nakatsutsumi　Self-infroduction</title>
+        <link rel="stylesheet" href="{{ asset('/css/test.css') }}">
     </head>
-        <body bgcolor= #6495ed>
-            <center>
+        <body>
+            <div class="circle">
                 {{$old}}
-                <div style="background:#7fffd4;
-                background-image: radial-gradient(#fff 10%,taransparent 10%);
-                background-size:10px 10px; background-position: 0 0, 10px
-                10px;   font-size: 100%; padding: 10px;">
-                 <h1>自己紹介</h1>
-                </div>
+            </div>
+            <div class="head-color text-center">
+                <h1>自己紹介</h1>
+            </div >
+            <div class="text-center">
                 <h2>名前</h2>
                 <p>中堤　智翔奈(なかつつみ　ちかな)</p>
                 <h2>誕生日</h2>
@@ -28,23 +28,24 @@
                 <img src= "omuraisu.png" width="200" height="100">
                 <h2>好きな動物</h2>
                 <p>犬</p>
-                    <img src="PANTI.jpg"width="300" height="200" alt="">
-                    <h2>趣味・特技</h2>
-                    <p>・プラモ作成</p>
-                    <p>・漫画を読む </p>
-                    <p>・アニメ・DVD鑑賞</p>
-                    <p>・音楽を聞くこと（J-POP,クラシック、アニソン　など）</p>
-                    <p>・剣道</p>
-                    <h2>一言コメント</h2>
-                    <strong><p>
-                            少々人見知りな部分もありますがお話などするのは好きなので<br>
-                            是非声をかけていただけると嬉しいです！<br>
-                            まだまだ未熟者ですがこれから色々覚えていって<br>
-                            少しでも早く皆さんの役に立てるようにしたいと思いますので                            
-                            ご指導よろしくお願いします。<br>
-                        </p></strong>
-            </center>
-
+                <img src="PANTI.jpg"width="300" height="200" alt="">
+                <h2>趣味・特技</h2>
+                <p>・プラモ作成</p>
+                <p>・漫画を読む </p>
+                <p>・アニメ・DVD鑑賞</p>
+                <p>・音楽を聞くこと（J-POP,クラシック、アニソン　など）</p>
+                <p>・剣道</p>
+                <h2>一言コメント</h2>
+                <strong>
+                    <p>
+                        少々人見知りな部分もありますがお話などするのは好きなので<br>
+                        是非声をかけていただけると嬉しいです！<br>
+                        まだまだ未熟者ですがこれから色々覚えていって<br>
+                        少しでも早く皆さんの役に立てるようにしたいと思いますので                            
+                        ご指導よろしくお願いします。<br>
+                    </p>
+                </strong>
+            </div>
             <a href="{{route('manga')}}"><img src="bakuman.jpg" alt="クリック"></a>
             <p>↑押すと漫画おすすめへ移動</p>
             
